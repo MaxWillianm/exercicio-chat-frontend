67c11272090c619f5d3365d7cdfe90813a772e34
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2023-11-15 10:44:58 -0300

    first commit

---
+++ src/components/skeleton.jsx
@@ -0,0 +1,9 @@
+export default function Skeleton() {
+  return (
+    <iframe
+      className="w-full h-screen"
+      src="https://chat.blip.ai/?appKey=ZGV2ZWxvcGVyOjQ2YzY1MjYtZjQzZC00ZmQ3LWI4ZjUtMjYyMjY4YjY5NzYx"
+      title="Atendimento Online"
+    />
+  )
+}
---
+++ src/routes/home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <div className="max-w-2xl mx-auto my-6">
+        <div className="bg-green-600 py-3 px-5 rounded-t-lg">
+          <h1 className="text-white text-2xl">Atendimento Online</h1>
+        </div>
+        <div className="h-scree flex items-end">
+
+        </div>
+      </div>
+    </>
+  )
+}
