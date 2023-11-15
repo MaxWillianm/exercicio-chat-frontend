786d024161a67c26eae13e294019eff641f08be2
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2023-11-15 10:58:36 -0300

    troca de rota

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './routes/home.jsx'
+import Chat from './routes/chat.jsx'
 
 
 const router = createBrowserRouter([
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/chat',
+        element: <Chat />,
       }
     ]
   },
