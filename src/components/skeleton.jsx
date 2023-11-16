139d9e4532d9814a20bb3d9464f7e396900831af
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2023-11-15 21:45:25 -0300

    apagando um componente

--- src/components/skeleton.jsx
+++
@@ -1,9 +0,0 @@
-export default function Skeleton() {
-  return (
-    <iframe
-      className="w-full h-screen"
-      src="https://chat.blip.ai/?appKey=ZGV2ZWxvcGVyOjQ2YzY1MjYtZjQzZC00ZmQ3LWI4ZjUtMjYyMjY4YjY5NzYx"
-      title="Atendimento Online"
-    />
-  )
-}
